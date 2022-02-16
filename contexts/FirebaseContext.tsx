777c41174f7ee1490133253856de112328fbbe5d
sahilbest999 <sahilpshahane123@gmail.com>
2022-02-17 02:28:35 +0530

    removed useless files casuing error

--- contexts/FirebaseContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import type { FirebaseApp } from 'firebase/app'
-import { getFirebaseInstance } from '@hooks/useFirebase'
-
-const app = getFirebaseInstance()
-const FirebaseContext = createContext<FirebaseApp>(app)
-console.log('getting context')
-export default FirebaseContext
